b9a442a5649414b4d740f833c0d82f5e4429095a
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-30 10:08:34 +0100

    Add project files.

---
+++ RPGSessionManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RPGSessionManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
