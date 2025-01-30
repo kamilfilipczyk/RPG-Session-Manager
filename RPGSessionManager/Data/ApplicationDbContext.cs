788343fa190d1d00c742de6a85fffed9ee2ea554
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-30 11:15:04 +0100

    Add DbContext

---
+++ RPGSessionManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPGSessionManager.Models;
+
+namespace RPGSessionManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Campaign> Campaigns { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
