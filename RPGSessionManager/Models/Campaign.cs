7e64788da57e43609edc5f4a9a098340aa2d093f
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-30 11:01:58 +0100

    Add Campaign model

---
+++ RPGSessionManager/Models/Campaign.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RPGSessionManager.Models
+{
+    public class Campaign
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; } = "";
+
+        [Required]
+        [MaxLength(500)]
+        public string About { get; set; } = "";
+
+        public bool HasEnded { get; set; } = false;
+
+        //relations
+        public List<Session> Sessions { get; set; } = new();
+    }
+}
