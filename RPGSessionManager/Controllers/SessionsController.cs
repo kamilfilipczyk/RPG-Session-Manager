5958ec6b154f24e58cf5734bafea601f4c1343f1
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-31 14:30:41 +0100

    Add SessionsContoller with views

---
+++ RPGSessionManager/Controllers/SessionsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RPGSessionManager.Data;
+using RPGSessionManager.Models;
+
+namespace RPGSessionManager.Controllers
+{
+    public class SessionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SessionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private bool SessionExists(int id)
+        {
+            return _context.Sessions.Any(e => e.Id == id);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Sessions.Include(s => s.Campaign).Include(s => s.Team);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .Include(s => s.Campaign)
+                .Include(s => s.Team)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["CampaignId"] = new SelectList(_context.Campaigns, "Id", "About");
+            ViewData["TeamId"] = new SelectList(_context.Teams, "Id", "About");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Title,About,Date,CampaignId,TeamId")] Session session)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(session);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CampaignId"] = new SelectList(_context.Campaigns, "Id", "About", session.CampaignId);
+            ViewData["TeamId"] = new SelectList(_context.Teams, "Id", "About", session.TeamId);
+            return View(session);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = _context.Sessions.Find(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            ViewData["CampaignId"] = new SelectList(_context.Campaigns, "Id", "About", session.CampaignId);
+            ViewData["TeamId"] = new SelectList(_context.Teams, "Id", "About", session.TeamId);
+            return View(session);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Title,About,Date,CampaignId,TeamId")] Session session)
+        {
+            if (id != session.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(session);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SessionExists(session.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CampaignId"] = new SelectList(_context.Campaigns, "Id", "About", session.CampaignId);
+            ViewData["TeamId"] = new SelectList(_context.Teams, "Id", "About", session.TeamId);
+            return View(session);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = _context.Sessions
+                .Include(s => s.Campaign)
+                .Include(s => s.Team)
+                .FirstOrDefault(m => m.Id == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sessions.Remove(session);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
