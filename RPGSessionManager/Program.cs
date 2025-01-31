5e3ec2295e46c5d3e50182e6030c8b693b34297d
Kamil <kamil.filipczyk.kontakt@gmail.com>
2025-01-31 17:09:05 +0100

    Remove comments from program.cs

--- RPGSessionManager/Program.cs
+++ RPGSessionManager/Program.cs
@@ -3,18 +3,15 @@ using RPGSessionManager.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=./Data/RPGSessionManager.db"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
